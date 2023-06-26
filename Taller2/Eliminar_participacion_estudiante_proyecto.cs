a48c7365604b3aed9c1ac0e1ee9806f14bda6e51
luciano90a <luciano_90lsv@hotmail.com>
2023-06-26 12:34:27 -0400

    Add project files.

---
+++ Taller2/Eliminar_participacion_estudiante_proyecto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller2
+{
+    public partial class Eliminar_participacion_estudiante_proyecto : Form
+    {
+        public Eliminar_participacion_estudiante_proyecto()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Taller2/Insertar_estudiante_carrera.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Taller2
+{
+    partial class Insertar_estudiante_carrera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(154, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insertar estudiante a carrera";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(80, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(484, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ingresar rut estudiante,El rut es sin guion, si el digito verificador es K se deb" +
+    "e reemplazar por un 0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(80, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingresar nombre de estudiante";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(80, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Insertar_estudiante_carrera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 420);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Broadway", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Insertar_estudiante_carrera";
+            this.Text = "Insertar_estudiante_carrera";
+            this.Load += new System.EventHandler(this.Insertar_estudiante_carrera_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+    }
+}
